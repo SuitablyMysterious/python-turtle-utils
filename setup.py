dd3c49c28da8f78b15e2fbf6d2dc52205fc87a51
SuitablyMysterious <fly2be@outlook.com>
2025-02-02 13:29:09 +0000

    rename library to python-turtle-utils and update description and author information

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mypythonlib',
+    name='python-turtle-utils',
     packages=find_packages(),
     version='0.1.0',
-    description='My first Python library',
-    author='Me',
+    description='A basic collection of utilities for the Python Turtle module',
+    author='SuitablyMysterious',
 )
