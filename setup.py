317e36874c08c8192f1ee771913704a9c829210c
SuitablyMysterious <fly2be@outlook.com>
2025-02-02 13:31:36 +0000

    restrict package inclusion to python-turtle-utils in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='python-turtle-utils',
-    packages=find_packages(),
+    packages=find_packages(include=['python-turtle-utils']),
     version='0.1.0',
     description='A basic collection of utilities for the Python Turtle module',
     author='SuitablyMysterious',
